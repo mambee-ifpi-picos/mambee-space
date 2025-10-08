cffefc424ff07702e16c33838ba6308d7a20cb84
EtoDoze <reverso12yt@gmail.com>
2025-10-08 11:21:18 -0300

    Editando a seção (sobre)

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,11 +1,13 @@
 export default function SobrePage() {
   return (
-    <section className="container mx-auto px-4 py-12">
-      <h1 className="text-3xl font-bold text-blue-700 mb-4">Sobre</h1>
-      <p className="text-gray-700 leading-relaxed">
+    <div className="flex flex-col min-h-screen w-screen">
+    <section className="flex-1 bg-[#C76E88] w-full p-8">
+      <h1 className="text-3xl font-bold !text-white mb-4">Como Funciona</h1>
+      <p className="text-white leading-relaxed">
         Este é o conteúdo da página <strong>Sobre</strong>. Aqui você pode
         adicionar informações sobre o site, a empresa ou você mesmo.
       </p>
     </section>
+    </div>
   );
 }
