ea0b413ac39092ba51154ab43c091dfa213947c7
jaderxnet <jaderxnet@gmail.com>
2025-12-15 18:33:00 -0300

    Correção relatório guarita

--- src/app/relatorio_guarita/page.tsx
+++ src/app/relatorio_guarita/page.tsx
@@ -95,85 +95,86 @@ export default function RelatorioPage() {
       <h1 className="text-4xl font-serif font-bold mb-8 text-black uppercase">
         Relatório
       </h1>
-      <div className="overflow-x-auto">
-        <table className="min-w-full border-separate border-spacing-y-4 text-sm">
-          <thead>
-            <tr className="text-left text-gray-600">
-              <th className="p-2 font-normal pl-6">Usuário</th>
-              <th className="p-2 font-normal">Sala</th>
-              <th className="p-2 font-normal">Espaço</th>
-              <th className="p-2 font-normal text-center">Horário</th>
-              <th className="p-2 font-normal text-center">Data</th>
-            </tr>
-          </thead>
-          <tbody>
-            {dados.length === 0 ? (
-              <tr>
-                <td
-                  colSpan={5}
-                  className="text-center p-6 text-gray-500 bg-gray-50 rounded"
-                >
-                  Nenhum resultado encontrado
-                </td>
+      <div className="mb-8">
+        <div className="overflow-x-auto">
+          <table className="min-w-full border-separate border-spacing-y-4 text-sm">
+            <thead>
+              <tr className="text-left text-gray-600">
+                <th className="p-2 font-normal pl-6">Usuário</th>
+                <th className="p-2 font-normal">Sala</th>
+                <th className="p-2 font-normal">Espaço</th>
+                <th className="p-2 font-normal text-center">Horário</th>
+                <th className="p-2 font-normal text-center">Data</th>
               </tr>
-            ) : (
-              dados.map((item) => (
-                <tr
-                  key={item.idReserva}
-                  className="group transition-transform hover:scale-[1.01]"
-                >
-                  <td className="bg-gray-50 p-4 rounded-l-lg border-l-[6px] border-teal-500 relative">
-                    <div className="flex items-center gap-3">
-                      {item.Usuario?.foto ? (
-                        <Image
-                          src={item.Usuario?.foto}
-                          alt={item.Usuario?.nome}
-                          width={40}
-                          height={40}
-                          className="w-10 h-10 rounded-full object-cover"
-                        />
-                      ) : (
-                        <Image
-                          src={`https://ui-avatars.com/api/?name=${encodeURIComponent(
-                            item.Usuario?.nome ?? "Usuário",
-                          )}&background=0c9488&color=fff&size=64`}
-                          alt={item.Usuario?.nome || "—"}
-                          width={40}
-                          height={40}
-                          className="w-10 h-10 rounded-full object-cover"
-                        />
-                      )}
-
-                      <div>
-                        <div className="text-gray-700 font-medium">
-                          {item.Usuario?.nome || "—"}
-                        </div>
-                      </div>
-                    </div>
-                  </td>
-
-                  <td className="bg-gray-50 p-4 text-gray-600">
-                    {item.Espaco?.Sala.nomeSala || "—"}
-                  </td>
-
-                  <td className="bg-gray-50 p-4 text-gray-600">
-                    {item.Espaco?.codigoEspaco || "—"}
-                  </td>
-
-                  <td className="bg-gray-50 p-4 text-center text-gray-600 font-medium">
-                    {formatarHora(item.horaInicio)} - {formatarHora(item.horaFim)}
-                  </td>
-
-                  <td className="bg-gray-50 p-4 text-center text-gray-600 rounded-r-lg">
-                    {formatarData(item.horaInicio) || "—"}
+            </thead>
+            <tbody>
+              {dados.length === 0 ? (
+                <tr>
+                  <td
+                    colSpan={5}
+                    className="text-center p-6 text-gray-500 bg-gray-50 rounded"
+                  >
+                    Nenhum resultado encontrado
                   </td>
                 </tr>
-              ))
-            )}
-          </tbody>
-        </table>
+              ) : (
+                dados.map((item) => (
+                  <tr
+                    key={item.idReserva}
+                    className="group transition-transform hover:scale-[1.01]"
+                  >
+                    <td className="bg-gray-50 p-4 rounded-l-lg border-l-[6px] border-teal-500 relative">
+                      <div className="flex items-center gap-3">
+                        {item.Usuario?.foto ? (
+                          <Image
+                            src={item.Usuario?.foto}
+                            alt={item.Usuario?.nome}
+                            width={40}
+                            height={40}
+                            className="w-10 h-10 rounded-full object-cover"
+                          />
+                        ) : (
+                          <Image
+                            src={`https://ui-avatars.com/api/?name=${encodeURIComponent(
+                              item.Usuario?.nome ?? "Usuário",
+                            )}&background=0c9488&color=fff&size=64`}
+                            alt={item.Usuario?.nome || "—"}
+                            width={40}
+                            height={40}
+                            className="w-10 h-10 rounded-full object-cover"
+                          />
+                        )}
+
+                        <div>
+                          <div className="text-gray-700 font-medium">
+                            {item.Usuario?.nome || "—"}
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+
+                    <td className="bg-gray-50 p-4 text-gray-600">
+                      {item.Espaco?.Sala.nomeSala || "—"}
+                    </td>
+
+                    <td className="bg-gray-50 p-4 text-gray-600">
+                      {item.Espaco?.codigoEspaco || "—"}
+                    </td>
+
+                    <td className="bg-gray-50 p-4 text-center text-gray-600 font-medium">
+                      {formatarHora(item.horaInicio)} - {formatarHora(item.horaFim)}
+                    </td>
+
+                    <td className="bg-gray-50 p-4 text-center text-gray-600 rounded-r-lg">
+                      {formatarData(item.horaInicio) || "—"}
+                    </td>
+                  </tr>
+                ))
+              )}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
